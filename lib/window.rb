703d0cb9a20d4613ffe2d1b15d155e3743008e61
laurcor55 <lcc33@duke.edu>
2019-01-31 14:58:37 -0500

    Feature/add waves (#6)
    * Adding waves file. Its not done. I just want to make sure its working
    
    * Moved images into media folder
    
    * Trying to make my git look like what i have
    
    * Updated waves with new format
    
    * I think I have to add these

--- lib/window.rb
+++ lib/window.rb
@@ -1,26 +1,29 @@
 require 'gosu'
 require_relative './input_control'
 
+
 class Window < Gosu::Window
   def initialize(player, width: 640, height: 480)
     super width, height
     self.caption = 'Sailor Game'
     @player = player
+    @waves = Waves.new
     @input = InputControl.new(@player)
   end
 
   def update
     dt = delta_time
     @input.update(dt)
+    @waves.update
     @player.update(dt) # update player
   end
 
   def draw
+    @waves.draw
     @player.draw # draw player
   end
 
   private
-
     def delta_time
       @last_elapsed ||= Gosu::milliseconds()
       elapsed = Gosu::milliseconds()
