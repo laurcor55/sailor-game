6236b5e50c72da620f6a56541429d90228cf9ec1
laurcor55 <lcc33@duke.edu>
2022-06-03 18:58:06 -0400

    Feature/fish disperse (#22)
    * school_of_fish.rb prints the x position of boat. This is test to see if I did it correctly
    
    * attr_reader
    
    * Player is communicating location to fish
    
    * School of fish disperse on space bar push. They now get less dense
    
    * School of fish shrinks in size slightly as disappearing
    
    * School of fish disperses when close to boat. Also disperses when press space bar
    
    * Fixed where dispersed is called.
    
    * dispersing functions done privately. Need to still discuss best way to delete the object
    
    * School of fish disperses when near. It comes back
    
    * Fish look good. Decreased motion_magnitude
    
    Co-authored-by: johnmgoodman <johnmgoodman@users.noreply.github.com>

--- lib/input_control.rb
+++ lib/input_control.rb
@@ -1,10 +1,13 @@
 require 'gosu'
 
 class InputControl
+
   MOTION_MAGNITUDE = 0.0001
 
-  def initialize(player_object)
+
+  def initialize(window_object, player_object)
     @player_object = player_object
+    @window_object = window_object
   end
 
   def update(dt)
